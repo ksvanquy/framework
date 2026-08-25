3859758677ddf0e376382011daae6f49b587927f
ksvanquy <ksvanquy@gmail.com>
2026-08-26 00:29:43 +0700

    Ignore local build and IDE artifacts

--- package-test/include/core/interfaces.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-namespace framework::core {
-// Chứa các interface cơ sở chung nếu có
-}
--- package-test/include/core/module_id.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace framework::core {
-using ModuleId = std::string;
-using ServiceId = std::string;
-} // namespace framework::core
